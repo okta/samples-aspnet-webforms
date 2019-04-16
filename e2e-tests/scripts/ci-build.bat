88a08cdc6aba4a73b86fdec6b371e030477eb465
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-04-16 14:36:28 -0700

    Addresses Roger's comment

--- e2e-tests/scripts/ci-build.bat
+++ e2e-tests/scripts/ci-build.bat
@@ -5,7 +5,7 @@ call npm install
 call npm run pretest
 
 cd "C:\Program Files (x86)\IIS Express"
-call start iisexpress.exe /path:%WORKSPACE%\MYGIT\okta-hosted-login\dist\okta-aspnet-webforms-example
+start iisexpress.exe /path:%WORKSPACE%\MYGIT\okta-hosted-login\dist\okta-aspnet-webforms-example
 
 cd C:\Users\builder\okta\samples-aspnet-webforms\e2e-tests\
 call npm run test:okta-hosted-login
@@ -13,7 +13,7 @@ if errorlevel 1 (SET error=true)
 call TASKKILL /F /IM iisexpress.exe
 
 cd "C:\Program Files (x86)\IIS Express"
-call start iisexpress.exe /path:%WORKSPACE%\MYGIT\self-hosted-login\dist\okta-aspnet-webforms-example
+start iisexpress.exe /path:%WORKSPACE%\MYGIT\self-hosted-login\dist\okta-aspnet-webforms-example
 
 cd C:\Users\builder\okta\samples-aspnet-webforms\e2e-tests\
 call npm run test:custom-login
