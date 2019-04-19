59b5360232f7f5611d3d5a8642046f397ec2c546
Vijet Mahabaleshwar <vijet.mahabaleshwar@okta.com>
2019-04-19 13:14:27 -0700

    Removed the trigger shells script

--- e2e-tests/scripts/ci-build.bat
+++ e2e-tests/scripts/ci-build.bat
@@ -1,18 +1,18 @@
-cd C:\Users\builder\okta\samples-aspnet-webforms\e2e-tests
+cd %SAMPLES_ASPNET_WEBFORMS_HOME%\e2e-tests
 call npm install
 call npm run pretest
 
-cd "C:\Program Files (x86)\IIS Express"
-call start iisexpress.exe /path:C:\Users\builder\okta\samples-aspnet-webforms\okta-hosted-login\dist\okta-aspnet-webforms-example
-cd C:\Users\builder\okta\samples-aspnet-webforms\e2e-tests
+cd %IIS_EXPRESS_HOME%
+call start iisexpress.exe /path:%SAMPLES_ASPNET_WEBFORMS_HOME%\okta-hosted-login\dist\okta-aspnet-webforms-example
+cd %SAMPLES_ASPNET_WEBFORMS_HOME%\e2e-tests
 call npm run test:okta-hosted-login
 if errorlevel 1 (SET error=true)
 call TASKKILL /F /IM iisexpress.exe
 
-cd "C:\Program Files (x86)\IIS Express"
-call start iisexpress.exe /path:C:\Users\builder\okta\samples-aspnet-webforms\self-hosted-login\dist\okta-aspnet-webforms-example
+cd %IIS_EXPRESS_HOME%
+call start iisexpress.exe /path:%SAMPLES_ASPNET_WEBFORMS_HOME%\self-hosted-login\dist\okta-aspnet-webforms-example
 
-cd C:\Users\builder\okta\samples-aspnet-webforms\e2e-tests
+cd %SAMPLES_ASPNET_WEBFORMS_HOME%\e2e-tests
 call npm run test:custom-login
 if errorlevel 1 (SET error=true)
 call TASKKILL /F /IM iisexpress.exe
