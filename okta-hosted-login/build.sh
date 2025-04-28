55eb142a6bfa02b2ecb371073cbfe645a2dab862
jiawenzhang-okta <jiawen.zhang@okta.com>
2025-04-28 14:56:37 -0700

    Removed ExcludeVersion

--- okta-hosted-login/build.sh
+++ okta-hosted-login/build.sh
@@ -77,7 +77,7 @@ if [ ! -f $PACKAGES_CONFIG_MD5 ] || [ "$( cat $PACKAGES_CONFIG_MD5 | sed 's/\r$/
     find . -type d ! -name . | xargs rm -rf
 fi
 
-mono "$NUGET_EXE" install -ExcludeVersion
+mono "$NUGET_EXE" install
 if [ $? -ne 0 ]; then
     echo "Could not restore NuGet packages."
     exit 1
